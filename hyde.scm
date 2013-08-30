a0b4ed9fb1902b8d74ede3b79081002922085bac
Nala Ginrut <nalaginrut@gmail.com>
2013-08-30 09:30:20 +0800

    add markdown

--- hyde.scm
+++ hyde.scm
@@ -1,3 +1,4 @@
-(use hyde)
+(use hyde lowdown)
  (output-dir "")
  (clean-before-build #f)
+(translators (cons (list "md" markdown->html) (translators)))
--- src/index.md
+++ src/index.md
@@ -1,3 +1,7 @@
 ((title . "index"))
 
+
 hello world
+===========
+
+* What is szdiy?
